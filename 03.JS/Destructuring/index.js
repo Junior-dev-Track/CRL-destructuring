773f821531945a5b437d1a90a6c93355641297c0
Pedrocito97 <pierre.willemart.110497@gmail.com>
2024-09-19 14:03:32 +0200

    Exo terminé

--- 03.JS/Destructuring/index.js
+++ 03.JS/Destructuring/index.js
@@ -8,9 +8,10 @@ Rewrite the code below to use array destructuring instead of assigning each valu
   
     let item = ["Egg", 0.25, 12];
   
-    let name = item[0];
-    let price = item[1];
-    let quantity = item[2];
+    // let name = item[0];
+    // let price = item[1];
+    // let quantity = item[2];
+    let [name, price, quantity] = item;
   
     console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
     console.log();
@@ -24,8 +25,9 @@ Rewrite the code below to use array destructuring instead of assigning each valu
     console.log("EXERCISE 2");
   
     let numbers = [3, 5, 4, 2, 6, 1];
-  
+    numbers.sort((a,b) => a - b);
     let [one, two, three, four, five, six] = numbers;
+
   
     console.log(`One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`);
     console.log();
@@ -45,7 +47,7 @@ Rewrite the code below to use array destructuring instead of assigning each valu
     let user = { name: "John", years: 30 };
   
     // your code to the left side:
-    let {} = user;
+    let {name, years : age, isAdmin = false} = user;
   
     console.log(name); // John
     console.log(age); // 30
@@ -61,10 +63,11 @@ Rewrite the code below to use array destructuring instead of assigning each valu
     console.log("EXERCISE 4");
   
     let person = [12, "Chris", "Owen"];
+    let [age, firstName, lastName] = person;
   
-    let firstName = person[1];
-    let lastName = person[2];
-    let age = person[0];
+    // let firstName = person[1];
+    // let lastName = person[2];
+    // let age = person[0];
   
     console.log(`Person - Age: ${age}, Name: ${firstName} ${lastName}`);
     console.log();
@@ -80,9 +83,11 @@ Rewrite the code below to use array destructuring instead of assigning each valu
     console.log("EXERCISE 5");
   
     let person = ["Chris", 12, "Owen"];
+
+    let [firstName, ,lastName] = person;
   
-    let firstName = person[0];
-    let lastName = person[2];
+    // let firstName = person[0];
+    // let lastName = person[2];
   
     console.log(`Name: ${firstName} ${lastName}`);
     console.log();
@@ -99,7 +104,7 @@ Rewrite the code below to use array destructuring instead of assigning each valu
     const students = ['Christina', 'Jon', 'Alexandare'];
   
     // Write your code here
-    const [] = students;
+    const [, , lastName] = students;
   
     console.log(lastName);
     console.log();
@@ -120,7 +125,7 @@ Rewrite the code below to use array destructuring instead of assigning each valu
     ];
   
     // Write your code here
-    const [] = moreStudents;
+    const [student1,[student2, student3],[student4,student5]] = moreStudents;
   
     console.log(student1, student2, student3, student4, student5);
     console.log();
@@ -148,14 +153,30 @@ Rewrite the code below to use array destructuring instead of assigning each valu
           ],
         },
       }
+      const {
+        title: title,
+        protagonist: {
+          name: protagonistName,
+          enemies: [
+            ,
+            ,
+            ,
+            {
+              name: enemyName,
+              title: enemyTitle,
+            }
+          ]
+        }
+      } = info;
       // const {} = info // <-- replace the next few `const` lines with this
-      const title = info.title
-      const protagonistName = info.protagonist.name
-      const enemy = info.protagonist.enemies[3]
-      const enemyTitle = enemy.title
-      const enemyName = enemy.name
-      return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`
+      // const title = info.title
+      // const protagonistName = info.protagonist.name
+      // const enemy = info.protagonist.enemies[3]
+      // const enemyTitle = enemy.title
+      // const enemyName = enemy.name
+
+      return `${enemyName} (${enemyTitle}) is an enemy to ${protagonistName} in "${title}"`;
     }
-    
+    console.log(nestedArrayAndObject());
   }
   
